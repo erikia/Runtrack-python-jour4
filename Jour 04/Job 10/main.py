91f5e68ca55a85b5af92b817f206eaca81649b23
erikia <nobileisabelle.in@gmail.com>
2023-03-09 12:58:49 +0100

    first commit

---
+++ Jour 04/Job 10/main.py
@@ -0,0 +1,10 @@
+def multiply_between():
+    L = [8, 24, 27, 48, 2,16, 9, 102, 7, 84, 91]
+
+    produit = 1 
+    for element in L:
+        produit *= element
+    print(produit)
+
+    
+multiply_between()
